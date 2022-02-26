92f551a855e24f8a6913421f1dd501bab57f66bc
Vadym Polianskyi <icvadym.polianskyi@americanwell.com>
2022-02-26 10:57:07 -0600

    Update config

--- src/config/config.py
+++ src/config/config.py
@@ -3,17 +3,16 @@ import os
 SERVER_HOST = os.environ.get('HOST', "0.0.0.0")
 SERVER_PORT = int(os.environ.get('PORT', 5000))
 
-BOT_API_TOKEN = "5107338273:AAE7IQ6nRotfuvdAmrbyrVws68HJMGr0j4Y"
+BOT_API_TOKEN = os.getenv("BOT_API_TOKEN")
 
-DB_HOST = 'db-mysql-alesha-amst-do-user-9731128-0.b.db.ondigitalocean.com'
-DB_PORT = 25060
-DB_NAME = 'alesha'
-DB_USERNAME = 'doadmin'
-DB_PASSWORD = 'ons4f71is9n17hef'
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = int(os.getenv("DB_PORT"))
+DB_NAME = os.getenv("DB_NAME")
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-DB_TABLE_ANNOUNCEMENT = 'evacuation_announcement'
+DB_TABLE_ANNOUNCEMENT = os.getenv("DB_TABLE_ANNOUNCEMENT")
 
-WEBHOOK_URL = 'https://ua-evacuation-bot.herokuapp.com/'
-
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 PARSE_MODE = "Markdown"
